a7b7b9ca0e001636cc6092d2f129f9a5677c41b5
Ovimonno Chy Roni <oceanc9361@gmail.com>
2022-09-05 22:28:02 +0600

    nav_tols

---
+++ apps.js
@@ -0,0 +1,11 @@
+function nav_tabs(){
+    // console.log("Click Marce")
+    document.getElementById('nurulAkher').style.display = "block";
+    document.getElementById("roniNath").style.display = 'none';
+
+}
+
+function nav_tabsRoni(){
+    document.getElementById("nurulAkher").style.display = "none";
+    document.getElementById("roniNath").style.display = 'block';
+}
